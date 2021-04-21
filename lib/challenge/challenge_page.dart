d9ff9f755ecd223fd2279c2744310382de469e3e
João Sátiro <joao.v.satiro@gmail.com>
2021-04-20 22:05:05 -0300

    create quiz and awnser

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
@@ -17,6 +18,7 @@ class _ChallengePageState extends State<ChallengePage> {
           child: QuestionIndicatorWidget(),
         )
       ),
+      body: QuizWidget(title: 'O que o Flutter faz em sua totalidade?')
     );
   }
 }
