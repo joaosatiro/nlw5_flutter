8f5d3e2d5cbb8cef41e5c53ba1e63ef8be36f240
João Sátiro <joao.v.satiro@gmail.com>
2021-04-22 23:24:21 -0300

    aula 4

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,23 +3,30 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
+  final int currentPage;
+  final int length;
+
+  const QuestionIndicatorWidget({
+    Key? key,
+    required this.currentPage,
+    required this.length,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Questão 04', style: AppTextStyles.body), 
-              Text('de 10', style: AppTextStyles.body)
-            ],
-          ),
-          SizedBox(height: 16),
-          ProgressIdicatorWidget(value: 0.7)
-        ]
-      ),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Questão $currentPage', style: AppTextStyles.body),
+            Text('de $length', style: AppTextStyles.body)
+          ],
+        ),
+        SizedBox(height: 16),
+        ProgressIdicatorWidget(value: currentPage / length)
+      ]),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/home/home_controller.dart';
 import 'package:DevQuiz/home/home_state.dart';
@@ -22,57 +23,67 @@ class _HomePageState extends State<HomePage> {
     controller.getUser();
     controller.getQuizzes();
     controller.stateNotifier.addListener(() {
-      setState((){});
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    if(controller.state == HomeState.succes) {
+    if (controller.state == HomeState.succes) {
       return Scaffold(
-        appBar: AppBarWidget(user: controller.user!),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 24
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(label: 'Fácil'),
-                  LevelButtonWidget(label: 'Médio'),
-                  LevelButtonWidget(label: 'Difícil'),
-                  LevelButtonWidget(label: 'Perito'),
-                ],
-              ),
-              SizedBox(
-                height: 24
-              ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  children: controller.quizzes!.map((e) => QuizCardWidget(
-                    title: e.title,
-                    percent: e.questionAnswered/e.questions.length,
-                    completed: '${e.questionAnswered}/${e.questions.length}',
-                  )).toList()
+          appBar: AppBarWidget(user: controller.user!),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(label: 'Fácil'),
+                    LevelButtonWidget(label: 'Médio'),
+                    LevelButtonWidget(label: 'Difícil'),
+                    LevelButtonWidget(label: 'Perito'),
+                  ],
                 ),
-              )
-            ],
-          ),
-        )
-      );
+                SizedBox(height: 24),
+                Expanded(
+                  child: GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      children: controller.quizzes!
+                          .map(
+                            (e) => QuizCardWidget(
+                              title: e.title,
+                              percent: e.questionAnswered / e.questions.length,
+                              completed:
+                                  '${e.questionAnswered}/${e.questions.length}',
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ChallengePage(
+                                      questions: e.questions,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          )
+                          .toList()),
+                ),
+              ],
+            ),
+          ));
     } else {
       return Scaffold(
-        body: Center(child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen)
-        ))
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
       );
     }
-    
   }
 }
